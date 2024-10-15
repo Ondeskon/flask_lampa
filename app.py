7ab6128428f0997fe9517c3c6e1fb07b9c4d929e
Ondeskon <118245966+Ondeskon@users.noreply.github.com>
2024-10-15 13:12:56 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    data = request.json
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({'id': new_item.id, 'name': new_item.name}), 201
+
+if __name__ == '__main__':
+    db.create_all()  # Creates database tables
+    app.run(debug=True)
